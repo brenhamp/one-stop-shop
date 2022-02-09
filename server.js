4db2b50d6bdff4e7d6cf0bece0759cd3cd1c6d32
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-02-08 22:04:51 -0600

    connection working-added comments to connection & server file

---
+++ server.js
@@ -0,0 +1,27 @@
+// Commenting out specific lines of code until ready for use
+
+// const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+// const routes = require('./controllers');
+// const exphbs = require('express-handlebars');
+const session = require('express-session');
+// const helpers = require('./utils/helpers');
+
+// const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
