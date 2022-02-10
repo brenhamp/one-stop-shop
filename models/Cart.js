f610707ee705238434f5bc054fa4d4c271deec5e
brenhamp <bpozzi@trinity.edu>
2022-02-09 18:42:05 -0600

    added shopping cart model

---
+++ models/Cart.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Cart extends Model {}
+
+Cart.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id'
+            }
+        }  
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'cart'
+    }
+);
+
+module.exports = Cart;
