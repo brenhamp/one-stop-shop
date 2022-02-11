b2b53f02a70a8014ad2c89e73679a068a5d481df
brenhamp <bpozzi@trinity.edu>
2022-02-10 19:15:18 -0600

    added user model and routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const departmentRoutes = require("./department-routes");
 const cartRoutes = require("./cart-routes");
+const userRoutes = require("./user-routes");
 
 router.use("/department", departmentRoutes);
 router.use("/cart", cartRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Department = require('./Department');
 const Product = require('./Product');
 const Cart = require('./Cart');
+const User = require('./User');
 
 // Product belongsto Department
 Product.belongsTo(Department);
@@ -10,8 +11,14 @@ Department.hasMany(Product, {
     foreignKey: 'department_id'
 });
 
+// Cart belongs to User
+Cart.belongsTo(User, {
+    foreignKey: 'cart_id'
+});
+
 module.exports = {
     Department,
     Product,
-    Cart
+    Cart,
+    User
 };
