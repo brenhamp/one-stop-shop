1efcff4ff6030150f1734314655a16b767f83050
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-02-10 19:33:41 -0600

    session created

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 // Commenting out specific lines of code until ready for use
 
-// const path = require('path');
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 // const helpers = require('./utils/helpers');
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Very Wow Secret Message',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
