3ce7c162c6dc735373d476430a33f8fcea171c14
Michael Bashford <mikebashford@gmail.com>
2022-02-15 22:11:14 -0600

    Added buttons for adding and subtracting quantities on the product card

---
+++ public/javascript/quantityButtons.js
@@ -0,0 +1,44 @@
+const minusSymbol = document.querySelector("#subtract");
+const addSymbol = document.querySelector("#add");
+const quantity = document.querySelector("#quantity");
+const addCartButton = document.querySelector("#cart");
+const quantityGroup = document.querySelector("#quantityManager");
+var itemCount = 0;
+
+console.log(itemCount);
+quantityGroup.setAttribute("style", "display:none;");
+
+function toggleAddCart() {
+  addCartButton.setAttribute("style", "display:none;");
+  itemCount++;
+  quantity.innerHTML = itemCount;
+  quantityGroup.setAttribute("style", "display:block");
+}
+
+function incrementItem() {
+  itemCount++;
+  console.log(itemCount);
+  quantity.innerHTML = itemCount;
+  if (itemCount === 0) {
+    addCartButton.setAttribute("style", "display:block;");
+  }
+}
+
+function decrementItem() {
+  if (itemCount > 0) {
+    itemCount--;
+    console.log(itemCount);
+  }
+  quantity.innerHTML = itemCount;
+  if (itemCount === 0) {
+    addCartButton.setAttribute(
+      "style",
+      "display:block; justify-content: center;"
+    );
+    quantityGroup.setAttribute("style", "display:none;");
+  }
+}
+
+minusSymbol.addEventListener("click", decrementItem);
+addSymbol.addEventListener("click", incrementItem);
+addCartButton.addEventListener("click", toggleAddCart);
