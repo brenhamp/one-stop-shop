c71a7b1f3bc02127b7f856d1fc25027103c6716f
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-02-17 17:43:49 -0600

    fixed homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Department, Product } = require("../models");
 
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
   
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
