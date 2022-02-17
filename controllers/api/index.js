71f308b65b071479e3f6aac07c916aa379fb47db
brenhamp <bpozzi@trinity.edu>
2022-02-16 21:48:00 -0600

    added working routes for department navigation links

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const departmentRoutes = require("./department-routes");
 const cartRoutes = require("./cart-routes");
 const userRoutes = require("./user-routes");
 
-router.use("/department", departmentRoutes);
 router.use("/cart", cartRoutes);
 router.use("/user", userRoutes);
 
